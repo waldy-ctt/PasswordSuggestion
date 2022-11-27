d8eca7545f593be6f7a0c9fd1dcf737b54aee7da
Waldy <92317021+waldy-ctt@users.noreply.github.com>
2022-11-27 16:36:15 +0700

    Add files via upload

---
+++ PasswordSuggestion/EnglishForm.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace PasswordSuggestion
+{
+    partial class EnglishForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txt1 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.Label();
+            this.LowerCaseLetter_ChkBx = new System.Windows.Forms.CheckBox();
+            this.UpperCaseLetter_ChkBx = new System.Windows.Forms.CheckBox();
+            this.Number_ChkBx = new System.Windows.Forms.CheckBox();
+            this.Symbol_ChkBx = new System.Windows.Forms.CheckBox();
+            this.SpecialCharacter_ChkBx = new System.Windows.Forms.CheckBox();
+            this.LenghthOfPassword_NumUpDown = new System.Windows.Forms.NumericUpDown();
+            this.txt3 = new System.Windows.Forms.Label();
+            this.CreatePassword_Button = new System.Windows.Forms.Button();
+            this.Password_TxtBx = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.Label();
+            this.txt5 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip4 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip5 = new System.Windows.Forms.ToolTip(this.components);
+            this.LanguageBox = new System.Windows.Forms.ComboBox();
+            this.txt6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.LenghthOfPassword_NumUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.AutoSize = true;
+            this.txt1.Location = new System.Drawing.Point(18, 9);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(183, 15);
+            this.txt1.TabIndex = 0;
+            this.txt1.Text = "Password Suggestion Application";
+            // 
+            // txt2
+            // 
+            this.txt2.AutoSize = true;
+            this.txt2.Location = new System.Drawing.Point(18, 31);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(284, 15);
+            this.txt2.TabIndex = 1;
+            this.txt2.Text = "Please choose what you want your password to have";
+            // 
+            // LowerCaseLetter_ChkBx
+            // 
+            this.LowerCaseLetter_ChkBx.AutoSize = true;
+            this.LowerCaseLetter_ChkBx.Location = new System.Drawing.Point(20, 49);
+            this.LowerCaseLetter_ChkBx.Name = "LowerCaseLetter_ChkBx";
+            this.LowerCaseLetter_ChkBx.Size = new System.Drawing.Size(119, 19);
+            this.LowerCaseLetter_ChkBx.TabIndex = 2;
+            this.LowerCaseLetter_ChkBx.Text = "Lower Case Letter";
+            this.toolTip1.SetToolTip(this.LowerCaseLetter_ChkBx, "a, b, c, d,...");
+            this.LowerCaseLetter_ChkBx.UseVisualStyleBackColor = true;
+            this.LowerCaseLetter_ChkBx.CheckedChanged += new System.EventHandler(this.CheckBox_Click);
+            // 
+            // UpperCaseLetter_ChkBx
+            // 
+            this.UpperCaseLetter_ChkBx.AutoSize = true;
+            this.UpperCaseLetter_ChkBx.Location = new System.Drawing.Point(183, 49);
+            this.UpperCaseLetter_ChkBx.Name = "UpperCaseLetter_ChkBx";
+            this.UpperCaseLetter_ChkBx.Size = new System.Drawing.Size(119, 19);
+            this.UpperCaseLetter_ChkBx.TabIndex = 3;
+            this.UpperCaseLetter_ChkBx.Text = "Upper Case Letter";
+            this.toolTip2.SetToolTip(this.UpperCaseLetter_ChkBx, "A, B, C, D,...");
+            this.UpperCaseLetter_ChkBx.UseVisualStyleBackColor = true;
+            this.UpperCaseLetter_ChkBx.CheckedChanged += new System.EventHandler(this.CheckBox_Click);
+            // 
+            // Number_ChkBx
+            // 
+            this.Number_ChkBx.AutoSize = true;
+            this.Number_ChkBx.Location = new System.Drawing.Point(20, 74);
+            this.Number_ChkBx.Name = "Number_ChkBx";
+            this.Number_ChkBx.Size = new System.Drawing.Size(70, 19);
+            this.Number_ChkBx.TabIndex = 4;
+            this.Number_ChkBx.Text = "Number";
+            this.toolTip3.SetToolTip(this.Number_ChkBx, "1, 2, 3, 4,...");
+            this.Number_ChkBx.UseVisualStyleBackColor = true;
+            this.Number_ChkBx.CheckedChanged += new System.EventHandler(this.CheckBox_Click);
+            // 
+            // Symbol_ChkBx
+            // 
+            this.Symbol_ChkBx.AutoSize = true;
+            this.Symbol_ChkBx.Location = new System.Drawing.Point(183, 74);
+            this.Symbol_ChkBx.Name = "Symbol_ChkBx";
+            this.Symbol_ChkBx.Size = new System.Drawing.Size(71, 19);
+            this.Symbol_ChkBx.TabIndex = 5;
+            this.Symbol_ChkBx.Text = "Symbols";
+            this.toolTip4.SetToolTip(this.Symbol_ChkBx, "!, @, #, $,...");
+            this.Symbol_ChkBx.UseVisualStyleBackColor = true;
+            this.Symbol_ChkBx.CheckedChanged += new System.EventHandler(this.CheckBox_Click);
+            // 
+            // SpecialCharacter_ChkBx
+            // 
+            this.SpecialCharacter_ChkBx.AutoSize = true;
+            this.SpecialCharacter_ChkBx.Location = new System.Drawing.Point(94, 99);
+            this.SpecialCharacter_ChkBx.Name = "SpecialCharacter_ChkBx";
+            this.SpecialCharacter_ChkBx.Size = new System.Drawing.Size(122, 19);
+            this.SpecialCharacter_ChkBx.TabIndex = 6;
+            this.SpecialCharacter_ChkBx.Text = "Special Characters";
+            this.toolTip5.SetToolTip(this.SpecialCharacter_ChkBx, "`, ~, (, ),...");
+            this.SpecialCharacter_ChkBx.UseVisualStyleBackColor = true;
+            this.SpecialCharacter_ChkBx.CheckedChanged += new System.EventHandler(this.CheckBox_Click);
+            // 
+            // LenghthOfPassword_NumUpDown
+            // 
+            this.LenghthOfPassword_NumUpDown.Location = new System.Drawing.Point(18, 156);
+            this.LenghthOfPassword_NumUpDown.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.LenghthOfPassword_NumUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.LenghthOfPassword_NumUpDown.Name = "LenghthOfPassword_NumUpDown";
+            this.LenghthOfPassword_NumUpDown.Size = new System.Drawing.Size(120, 23);
+            this.LenghthOfPassword_NumUpDown.TabIndex = 7;
+            this.LenghthOfPassword_NumUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // txt3
+            // 
+            this.txt3.AutoSize = true;
+            this.txt3.Location = new System.Drawing.Point(18, 138);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(198, 15);
+            this.txt3.TabIndex = 8;
+            this.txt3.Text = "Please choose your password length";
+            // 
+            // CreatePassword_Button
+            // 
+            this.CreatePassword_Button.Enabled = false;
+            this.CreatePassword_Button.Location = new System.Drawing.Point(18, 199);
+            this.CreatePassword_Button.Name = "CreatePassword_Button";
+            this.CreatePassword_Button.Size = new System.Drawing.Size(173, 23);
+            this.CreatePassword_Button.TabIndex = 9;
+            this.CreatePassword_Button.Text = "Create Password";
+            this.CreatePassword_Button.UseVisualStyleBackColor = true;
+            this.CreatePassword_Button.Click += new System.EventHandler(this.CreatePassword_Button_Click);
+            // 
+            // Password_TxtBx
+            // 
+            this.Password_TxtBx.Location = new System.Drawing.Point(18, 251);
+            this.Password_TxtBx.Name = "Password_TxtBx";
+            this.Password_TxtBx.ReadOnly = true;
+            this.Password_TxtBx.Size = new System.Drawing.Size(309, 23);
+            this.Password_TxtBx.TabIndex = 10;
+            // 
+            // txt4
+            // 
+            this.txt4.AutoSize = true;
+            this.txt4.Location = new System.Drawing.Point(15, 233);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(87, 15);
+            this.txt4.TabIndex = 11;
+            this.txt4.Text = "Your password:";
+            // 
+            // txt5
+            // 
+            this.txt5.AutoSize = true;
+            this.txt5.Location = new System.Drawing.Point(18, 286);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(256, 15);
+            this.txt5.TabIndex = 12;
+            this.txt5.Text = "Your password have been copied to clipboard !!";
+            this.txt5.Visible = false;
+            // 
+            // LanguageBox
+            // 
+            this.LanguageBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.LanguageBox.FormattingEnabled = true;
+            this.LanguageBox.Items.AddRange(new object[] {
+            "English",
+            "Tiếng Việt"});
+            this.LanguageBox.Location = new System.Drawing.Point(18, 327);
+            this.LanguageBox.Name = "LanguageBox";
+            this.LanguageBox.Size = new System.Drawing.Size(121, 23);
+            this.LanguageBox.TabIndex = 13;
+            this.LanguageBox.SelectedIndexChanged += new System.EventHandler(this.SetLanguage);
+            // 
+            // txt6
+            // 
+            this.txt6.AutoSize = true;
+            this.txt6.Location = new System.Drawing.Point(18, 309);
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(62, 15);
+            this.txt6.TabIndex = 14;
+            this.txt6.Text = "Language:";
+            // 
+            // EnglishForm
+            // 
+            this.AcceptButton = this.CreatePassword_Button;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(342, 395);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.LanguageBox);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.Password_TxtBx);
+            this.Controls.Add(this.CreatePassword_Button);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.LenghthOfPassword_NumUpDown);
+            this.Controls.Add(this.SpecialCharacter_ChkBx);
+            this.Controls.Add(this.Symbol_ChkBx);
+            this.Controls.Add(this.Number_ChkBx);
+            this.Controls.Add(this.UpperCaseLetter_ChkBx);
+            this.Controls.Add(this.LowerCaseLetter_ChkBx);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "EnglishForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "P.S";
+            ((System.ComponentModel.ISupportInitialize)(this.LenghthOfPassword_NumUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label txt1;
+        private Label txt2;
+        private CheckBox LowerCaseLetter_ChkBx;
+        private CheckBox UpperCaseLetter_ChkBx;
+        private CheckBox Number_ChkBx;
+        private CheckBox Symbol_ChkBx;
+        private CheckBox SpecialCharacter_ChkBx;
+        private NumericUpDown LenghthOfPassword_NumUpDown;
+        private Label txt3;
+        private Button CreatePassword_Button;
+        private TextBox Password_TxtBx;
+        private Label txt4;
+        private Label txt5;
+        private ToolTip toolTip1;
+        private ToolTip toolTip2;
+        private ToolTip toolTip3;
+        private ToolTip toolTip4;
+        private ToolTip toolTip5;
+        private ComboBox LanguageBox;
+        private Label txt6;
+    }
+}
---
+++ PasswordSuggestion/EnglishForm.cs
@@ -0,0 +1,180 @@
+﻿using System.Drawing.Text;
+using System.Media;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PasswordSuggestion
+{
+    public partial class EnglishForm : Form
+    {
+        public string[] password { get; set; }
+
+        private string[] LowerCaseLetter = new string[] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        private string[] UpperCaseLetter = new string[] {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        private string[] NumberLeter = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        private string[] Symbols = new string[] { "!", "@", "#", "$", "%", "^", "&", "*" };
+        private string[] SpecialLetter = new string[] { "(", ")", "{", "}", ":", ";", "'", "\"", "\\", "|", "?", "/", ".", ">", "<", ",", "~", "`"};
+
+        public EnglishForm()
+        {
+            InitializeComponent();
+            LanguageBox.SelectedIndex = 0;
+        }
+
+        private void CreatePassword_Button_Click(object sender, EventArgs e)
+        {
+            SoundPlayer son = new SoundPlayer(@"E:\Code\C#\Project\PasswordSuggestion\click.wav");
+            son.Play();
+
+            string[] password = new string[Convert.ToInt16(LenghthOfPassword_NumUpDown.Value)];
+            Random random = new Random();
+
+            for(int i = 0; i < password.Length; i++)
+            {
+                switch(random.Next(5))
+                {
+                    case 0:
+                        if(LowerCaseLetter_ChkBx.Checked == true)
+                        {
+                            password[i] = LowerCaseLetter[random.Next(UpperCaseLetter.Length)];
+                            break;
+                        }
+                        else
+                        {
+                            i--;
+                            break;
+                        }
+
+                    case 1:
+                        if(UpperCaseLetter_ChkBx.Checked == true)
+                        {
+                            password[i] = UpperCaseLetter[random.Next(LowerCaseLetter.Length)];
+                            break;
+                        }
+                        else
+                        {
+                            i--;
+                            break;
+                        }
+
+                    case 2:
+                        if(Number_ChkBx.Checked == true)
+                        {
+                            password[i] = NumberLeter[random.Next(NumberLeter.Length)];
+                            break;
+                        }
+                        else
+                        {
+                            i--;
+                            break;
+                        }
+
+                    case 3:
+                        if(Symbol_ChkBx.Checked == true)
+                        {
+                            password[i] = Symbols[random.Next(Symbols.Length)];
+                            break;
+                        }
+                        else
+                        {
+                            i--;
+                            break;
+                        }
+
+                    case 4:
+                        if(SpecialCharacter_ChkBx.Checked == true)
+                        {
+                            password[i] = SpecialLetter[random.Next(SpecialLetter.Length)];
+                            break;
+                        }
+                        else
+                        {
+                            i--;
+                            break;
+                        }
+                }
+            }
+
+            this.password = password;
+            this.Password_TxtBx.Text = string.Concat(this.password);
+            this.Password_TxtBx.Refresh();
+
+            if(Password_TxtBx.Text != string.Empty)
+            {
+                System.Windows.Forms.Clipboard.SetText(string.Concat(this.password));
+                txt5.Visible = true;
+            }
+        }
+
+        private void CheckBox_Click(object sender, EventArgs e)
+        {
+            if(LowerCaseLetter_ChkBx.Checked == false && UpperCaseLetter_ChkBx.Checked == false && Number_ChkBx.Checked == false && Symbol_ChkBx.Checked == false && SpecialCharacter_ChkBx.Checked == false)
+            {
+                CreatePassword_Button.Enabled = false;
+            }
+            else
+            {
+                CreatePassword_Button.Enabled = true;
+            }
+        }
+
+        private void SetLanguage(object sender, EventArgs e)
+        {
+            if(LanguageBox.SelectedIndex == 1)
+            {
+                txt1.Text = "Ứng dụng gợi ý mật khẩu";
+                txt2.Text = "Hãy chọn những thứ bạn muốn có trong mật khẩu";
+                LowerCaseLetter_ChkBx.Text = "Chữ cái thường";
+                UpperCaseLetter_ChkBx.Text = "Chữ cái in hoa";
+                Number_ChkBx.Text = "Số";
+                Symbol_ChkBx.Text = "Kí tự đặc biệt bậc 1";
+                SpecialCharacter_ChkBx.Text = "Kí tự đặc biệt bậc 2";
+                txt3.Text = "Hãy chọn độ dài mật khẩu";
+                CreatePassword_Button.Text = "Tạo mật khẩu";
+                txt4.Text = "Mật khẩu của bạn:";
+                txt5.Text = "Mật khẩu của bạn đã được copy";
+                txt6.Text = "Ngôn ngữ:";
+
+                txt1.Update();
+                txt2.Update();
+                txt3.Update(); 
+                txt4.Update(); 
+                txt5.Update();
+                txt6.Update();
+                LowerCaseLetter_ChkBx.Update();
+                UpperCaseLetter_ChkBx.Update();
+                Number_ChkBx.Update();
+                Symbol_ChkBx.Update();
+                SpecialCharacter_ChkBx.Update();
+                CreatePassword_Button.Update();
+            }
+            else if(LanguageBox.SelectedIndex == 0)
+            {
+                txt1.Text = "Password Suggestion Application";
+                txt2.Text = "Please choose what you want your password to have";
+                LowerCaseLetter_ChkBx.Text = "Lower Case Letter";
+                UpperCaseLetter_ChkBx.Text = "Upper Case Letter";
+                Number_ChkBx.Text = "Number";
+                Symbol_ChkBx.Text = "Symbols";
+                SpecialCharacter_ChkBx.Text = "Special Characters";
+                txt3.Text = "Please choose your password length";
+                CreatePassword_Button.Text = "Create Password";
+                txt4.Text = "Your password:";
+                txt5.Text = "Your password have been copied to clipboard";
+                txt6.Text = "Language:";
+
+                txt1.Update();
+                txt2.Update();
+                txt3.Update();
+                txt4.Update();
+                txt5.Update();
+                txt6.Update();
+                LowerCaseLetter_ChkBx.Update();
+                UpperCaseLetter_ChkBx.Update();
+                Number_ChkBx.Update();
+                Symbol_ChkBx.Update();
+                SpecialCharacter_ChkBx.Update();
+                CreatePassword_Button.Update();
+            }
+        }
+    }
+}
